f3986a5e6f97431d6c8fecc829d8b3e58003416f
Diego De Granda <degranda@gmail.com>
2020-01-27 18:13:26 -0600

    changes on tchallenge 03

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 const factorial = (number) => {
-    let result = 1;
-  
-    for (let i = 2; i <= number; i += 1) {
-      result *= i;
-    }
-  
-    return result;
+    
   }
 
   module.exports = factorial;
