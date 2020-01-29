1839367e7af5f09525f875dbfffe57b4864f7563
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-01-29 09:17:09 -0600

    Challenge solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
-    
+  let result;
+  if (number === 0) {
+    result = 1;
+  } else {
+    result = number * factorial(number - 1);
   }
+  return result;
+}
 
-  module.exports = factorial;
+module.exports = factorial;
