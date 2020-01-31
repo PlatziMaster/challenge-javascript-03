22b4282854eb09d10d75c0ef8f1464e1e24f8d0a
Oscar Barajas Tavares <oscar@arepa.dev>
2020-01-30 23:59:16 -0500

    challenge of factorial was resolved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-const factorial = (number) => {
+ const factorial = (number) => {
     
+    if (!this.cache) {
+      this.cache = {}
+    }
+    
+    if (this.cache[number]) {
+      return this.cache[number]
+    }
+
+    if (number === 1 || number === 0) {
+      return 1
+    }    
+
+    this.cache[number] = number * factorial(number - 1)
+    return this.cache[number]
   }
 
   module.exports = factorial;
+
+ 
