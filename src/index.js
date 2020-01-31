f5ddee27564aa48327e5c1817faa4944338b7f1b
Wilson Romero <wilson.romero@gmail.com>
2020-01-31 08:57:09 -0500

    Solucion al reto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-const factorial = (number) => {
-    
+const factorial = number => {
+  if (number === 0 || number === 1) return 1;
+  for (var i = number - 1; i >= 1; i--) {
+    number *= i;
   }
+  return number;
+};
 
-  module.exports = factorial;
+module.exports = factorial;
