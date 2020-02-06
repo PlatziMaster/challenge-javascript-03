6d12b7996041bd491102d7d649a1cd07c2cadae0
eriksape <erik.sape@gmail.com>
2020-02-05 23:30:26 -0600

    solving factorial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const factorial = (number) => {
-    
-  }
+  let result = 1, i = number;
+  for (let i = number; i >= 2; i--) result = result * i;
+  return result;
+}
 
-  module.exports = factorial;
+module.exports = factorial;
