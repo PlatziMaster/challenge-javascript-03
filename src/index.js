579b85df19b6005ac4babb8578dded80a713c744
Jefferson Aguilar <jefferson@alunices.com>
2020-02-05 21:54:58 -0500

    Implementación de funcion factorial()

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-    
+  let factoriaNumber = 1;
+  for (let index = 1; index <= number; index++) {
+    factoriaNumber = factoriaNumber * index;
   }
+  return factoriaNumber;
+}
 
-  module.exports = factorial;
+module.exports = factorial;
