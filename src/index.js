90bdac6df594729dcc12d23a40c5fbed9e93498f
MauricioFa <mauriciositel@hotmail.com>
2020-02-12 17:54:10 -0500

    Factorial challenge solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-    
+  let x = 1
+  for (let i = 1; i <= number; i++) { //Doesn´t execute when number = 0 (i <= number is false at first time)
+    x = x * i
   }
+  return x
+}
 
-  module.exports = factorial;
+module.exports = factorial;
