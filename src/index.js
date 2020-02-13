63d4e28d5ae1125f6aa3cda064f443564087f8da
FrcGustavo <hidalgo_fco520@outlook.com>
2020-02-13 16:33:53 -0600

    Redo terminado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-    
-  }
+  let fact = 1;
+  for(number; number > 0; number--){
+    fact = fact * number;
+  };
+  return fact;
+}
 
-  module.exports = factorial;
+module.exports = factorial;
