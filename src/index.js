c99d4137b78db02cd65ea9815d22ab4ac9b1576b
luis_parra <luis.parra.pulido@gmail.com>
2020-02-20 22:58:45 -0500

    reto terminado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const factorial = (number) => {
-    
+  const n = number;
+  let total = 1;
+  for(i=1; i<=n; i++) {
+      total = total*i
+    }
+    return total;
   }
 
   module.exports = factorial;
