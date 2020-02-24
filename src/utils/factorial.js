aa6fd4fb5272d6d985d505beb142ca0cd109e540
David Behar <david@behar.io>
2020-02-23 23:10:47 -0600

    Recursive factorial

--- src/utils/factorial.js
+++ src/utils/factorial.js
@@ -1,10 +1,6 @@
 const factorial = (number) => {
-  let result = 1
-  for(let i = 0;i<number;i++){
-    console.log(result)
-    result=result*(i+1)
-  }
-  return result
+  if(number===0) return 1
+  return number * factorial(number-1)
 }
 
 module.exports = factorial;
