db0360d502fab8c0ee0e1ece6e2b02d006e5d7e9
Alejandro Lopez <alejozepol@gmail.com>
2020-03-27 14:50:06 -0500

    solucion de reto elaboración de factorial.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const factorial = (number) => {
-    
+  let result
+    if(number === 0 || number === 1){
+      result = 1
+    }
+    else{
+      result = number
+      for (let index = 1; index < number; index++) {
+        result = result * (number - index)
+      }
+    }
+    console.log(`El factor de ${number} es ${result}`)
+    return result
   }
+
+  factorial(5)
 
   module.exports = factorial;
