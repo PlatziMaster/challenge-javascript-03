153e100f37d178af5346108fcb1d2bd2116e3a2c
J. Alvarez <jcalvarezj@gmail.com>
2020-04-02 17:08:13 -0500

    Solved Challenge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const factorial = (number) => {
-    
-  }
+    let result = 1;
+    for (let i = 1; i <= number; i++)
+      result *= i;
+    return result;
+}
 
-  module.exports = factorial;
+module.exports = factorial;
