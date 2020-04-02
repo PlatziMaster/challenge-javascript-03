e976094172a0eb6a0e9cb1d54a5fac35c4c8f9b2
Alex <ajf@fastmail.com>
2020-04-02 10:31:51 -0600

    función que devuelve numero factorial que se le pase como parametro.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const factorial = (number) => {
-    
+    let result = 1;
+    if (number === 0) {
+      return result;
+    }
+    for (let i = 1; i <= number; i++) {
+      result = result * i;
+    }
+    return result;
   }
 
   module.exports = factorial;
