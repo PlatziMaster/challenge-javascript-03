1643ce663d981c4c87e89c2483585984bd6512f4
Luis Caraballo <luisred61@gmail.com>
2020-04-01 23:09:01 -0500

    Challenge done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-    
-  }
+  let result = 1;
+  for (let i = 1; i <= number; i++)
+    result *= i;
 
-  module.exports = factorial;
+  return result;
+}
+
+module.exports = factorial;
