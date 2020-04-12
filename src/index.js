8f282b5aed94adeb626d736accef71543e518455
hectormr <45437401+HectorMartinezResendiz@users.noreply.github.com>
2020-04-12 12:34:22 -0500

    Resolviendo el challenge de javascript 03 con recursividad

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
-    
-  }
+  // let j = 1;
+  // for (i = 1; i <= number; i++) {
+  //   j *= i;
+  // }
+  // return j;
+  if (number <= 1) return 1;
+  return number * factorial(number - 1);
+}
 
-  module.exports = factorial;
+module.exports = factorial;
