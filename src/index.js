7bac6a951fdc943a9d014402b49033d8821f7657
camilo <velandiacamilo356@gmail.com>
2020-04-20 20:41:54 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
-    
+  let fac = 1;
+  let a = 1;
+  for (let i = 0; i < number; i++) {
+    fac = (i + 1) * a;
+    a = fac;
   }
+  return fac;
+};
 
-  module.exports = factorial;
+module.exports = factorial;
