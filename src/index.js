9f333f6d701ab3e978d7bc05eb14f333289976d4
Iván Chipatecua <ivchip@gmail.com>
2020-04-25 14:58:14 -0500

    Challenge javascript 03 completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
-    
+  let factorial = 1;
+  if (number > 0) {
+    for (let index = 1; index <= number; index++) {
+      factorial *= index
+    }
   }
+  return factorial;
+}
 
-  module.exports = factorial;
+module.exports = factorial;
