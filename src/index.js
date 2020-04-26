8e76ea6e703596f54d46baaacb0debc9ca2199fb
Tonalli López <aguilablanca_7@live.com>
2020-04-26 17:22:17 -0500

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const factorial = (number) => {
-    
+  if (number === 0) {
+    return 1;
   }
+  return (number * factorial(number - 1));
+}
 
-  module.exports = factorial;
+module.exports = factorial;
