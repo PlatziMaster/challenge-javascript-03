19e62ee667b04efd7534f0a03149a129b40f84a2
Razielini <razielco@gmail.com>
2020-05-05 19:25:38 -0600

    Update index.js
    solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-    
+  let res = 1
+  for(i=1; i<=number; i++) {
+    res = res * i
   }
+  return res
+}
 
-  module.exports = factorial;
+module.exports = factorial;
