ab2fbbab71be978e07af077b0ef1ccfec13a0223
Agustin Moran <agustin.moranr@gmail.com>
2020-05-10 14:20:50 -0500

    Se completo el reto y se definio el concepto de recursividad

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const factorial = (number) => {
-    
-  }
+const values = [0, 1, 5, 8, 10];
 
-  module.exports = factorial;
+// const factorial = (number) => {
+//   if(number === 0) {
+//     return 1;
+//   };
+//   else {
+//     return number * factorial(number - 1); // recursivity 
+//   };
+// };
+
+const factorial = number => number === 0? 1 : number * factorial(number - 1) // cleanest way
+
+factorial(values.length);
+
+module.exports = factorial;
