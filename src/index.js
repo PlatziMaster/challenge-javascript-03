c9a03f3a3899a206443b181b6f8cd2bad825c9f4
lenninIbarraGonzalez <soporte@allnexus.com.co>
2020-05-11 08:54:15 -0500

    eliminando comentarios

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ const factorial = (number) => {
   for (let i = 1; i <= number; i++) {
     acum = acum * i;
   }
-  // console.log(acum);
   return acum;
 };
-
-factorial(5);
 
 module.exports = factorial;
