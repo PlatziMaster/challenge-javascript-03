8f5f46d2309d27dda1f2a29e252ec8e96b022584
gposso <gilberto.posso@gmail.com>
2020-05-11 21:10:22 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const factorial = (number) => {
-    
+  let result = 1;
+  while (number > 0) {
+    result *= number;
+    number--;
   }
+  return result;
+}
 
-  module.exports = factorial;
+module.exports = factorial;
