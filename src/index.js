35da601f9686105a3083d81916b2804bc2f7af8f
Urieel Mejia <uriel.mejia.unitec@outlook.com>
2020-05-15 14:55:44 -0500

    Solución al challenge JavaScript 03

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const factorial = (number) => {
-    
+  resultado = 1;
+  let i = 1;
+  while (i <= number) {
+    resultado = resultado * i;
+    i++;
   }
+  return resultado;
+};
 
-  module.exports = factorial;
+console.log(factorial(15));
+module.exports = factorial;
