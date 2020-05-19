7b093ca5bd85bb222549c3e5f845938a44c995a7
Andres Valencia <cryos74@gmail.com>
2020-05-19 16:27:32 -0500

    challenge-javascript-03 passed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
-    
-  }
+	let factor = 1
+	for (let i = 1; i <= number; i++) {
+    if (i < 2) {
+      factor = 1
+    } else {
+	    factor = factor * i
+    }
+	}
+	return factor
+}
 
   module.exports = factorial;
