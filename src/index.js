4ab91f3f97d3b914f0c01d1d8a25dcce865ea5e8
Felipe Merchán <felipe-sc@hotmail.es>
2020-05-22 18:21:14 -0500

    I resolved my challenge 03 with memorization

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-const factorial = (number) => {
-    
+const factorial = number => {
+  if(!this.cache) {
+    this.cache = {}
   }
 
-  module.exports = factorial;
+  if (this.cache[number]) {
+    return this.cache[number]
+  }
+
+  if (number === 0 || number === 1) {
+    return 1
+  }
+
+  this.cache[number] = number * factorial(number - 1)
+  return this.cache[number]
+}
+
+module.exports = factorial;
