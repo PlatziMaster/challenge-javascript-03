017d2ec44f7491af759e9b791d18ffe3384d4c60
Juan Gaybre <jugaybre@gmail.com>
2020-05-22 20:52:04 -0500

    Reto: Factorial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-const factorial = (number) => {
-    
+const factorial = number => {
+  var res =1
+  if(number > 1) {
+    for(let i=number; i>0; i--)
+      res *= i
   }
+  return res
+}
 
-  module.exports = factorial;
+module.exports = factorial
+
+/*
+Este codigo tambien pasó el test pero después lo reducí con un for
+
+var res = 1
+if(number > 1) {
+  do{
+    res *= number
+    number--
+  }while(number !=0)
+}
+return res
+*/
