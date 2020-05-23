83da0f0a95a4bc242375d7b3be19a0c1dcd715a0
Osvaldo Arzate <osvarz.new@gmail.com>
2020-05-23 00:07:37 -0500

    Se arregla para que funcione con el 0

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const factorial = (number) => {
   var numFactorial = 1;
   for (var i = 0; i <= number; i++) {
     if (i === 0) {
-      numFactorial = 1
+      numFactorial = 1;
     } else {
-      numFactorial = (i + 1) * numFactorial;
+      numFactorial = numFactorial * i;
     }
   }
   return numFactorial;
