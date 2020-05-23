b3578ad02795f59a0d0398396752a7baff272228
Osvaldo Arzate <osvarz.new@gmail.com>
2020-05-22 22:54:18 -0500

    Se termina reto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
-    
+  var numFactorial = 1;
+  for (var i = 0; i <= number; i++) {
+    if (i === 0) {
+      numFactorial = 1
+    } else {
+      numFactorial = (i + 1) * numFactorial;
+    }
   }
+  return numFactorial;
+}
 
-  module.exports = factorial;
+module.exports = factorial;
