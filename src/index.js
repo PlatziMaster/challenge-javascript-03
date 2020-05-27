6716ae22ee406b39b6b2d02aba2970820948c8d5
alejandromunozc <alejandromunozc@gmail.com>
2020-05-26 21:04:32 -0500

    Función que devuelve el factorial de un numero ingresado.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-    
-  }
+    let fact = 1;
+    for (let i = 2; i <= number; i++) {
+        fact *= i;
+    }
+    return fact;
+}
 
-  module.exports = factorial;
+module.exports = factorial;
