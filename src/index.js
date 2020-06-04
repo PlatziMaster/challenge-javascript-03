1ad069cdefb69a5015a5ab607b7eff0cda0a41b5
Luis LC <luis.lazcanocruz@gmail.com>
2020-06-03 23:54:18 -0500

    Reto Cumplido

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const factorial = (number) => {
-    
-  }
 
-  module.exports = factorial;
+  return factorialFormule(number);
+
+  function factorialFormule(f){
+    if(!this.cache){
+      this.cache = {}
+    }
+
+    if(this.cache[f]){
+      return this.cache[f]
+    }
+
+    if(f <= 1){
+      return 1;
+    }
+    else{
+      this.cache[f] = f * factorialFormule(f-1);
+      return this.cache[f];
+    }
+  }
+}
+
+module.exports = factorial;
