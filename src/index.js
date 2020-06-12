41cce535fbb0773e9efcc4cb5d860fd2d71282a5
Daniel Arrubla <darrubla@uninorte.edu.co>
2020-06-12 10:28:29 -0500

    factorial challenge solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const factorial = (number) => {
-    
-  }
+  let result = number;
 
-  module.exports = factorial;
+  if (number === 0) {
+    result = 1;
+  } else {
+    for (let i = 1; i < number; i++) {
+      result *= i;
+    }
+  }
+  return result;
+};
+
+module.exports = factorial;
