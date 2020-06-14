23204e02925b2ddebedc26acdf423d4e4e76abec
Alejandro Barba <alejandrobarba742@gmail.com>
2020-06-14 00:40:27 -0500

    Reto resuelto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const factorial = (number) => {
-    
+  let total = 1;
+    if(number === 0 || number === 1){
+      return 1;
+    }
+    for(let i = number;i > 0; i--){
+      total = total * i;
+    }
+    return total;
   }
 
   module.exports = factorial;
