a0a1d79cc7e3bc78ae1734ed82e579bb5f34e25f
Josh Wolf <alienxtrix@gmail.com>
2020-06-17 15:49:12 -0500

    complete challenge 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
-    
+  let fact = 1;
+  for (let i = 1; i <= number; i++) {
+    fact = fact * i;
   }
+  return fact;
+};
+      
+ 
+console.log(factorial(5));
+
 
   module.exports = factorial;
