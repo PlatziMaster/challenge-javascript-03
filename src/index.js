e4f2088aabdf7f56f0b5a8859d3183b085dbf433
Jesus Mejia <chucho4720013@gmail.com>
2020-06-20 00:41:57 -0500

    Reto resuelto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
-    
+  let resultado = 1
+
+  for (let i = 1; i <= number; i++) {
+    resultado *= i  
+  }
+
+  return resultado
   }
 
   module.exports = factorial;
