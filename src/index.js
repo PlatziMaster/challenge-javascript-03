74ffb05ef5c22bb86c0f6ece4e704bdc54d1b05e
cesar <csgalindos@hotmail.com>
2020-06-21 16:40:18 -0500

    [updated] solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
-    
+  let fact = 1
+  let i = 1
+  while (i <= number) {
+    fact = fact * i
+    i += 1
+  }
+  return fact
   }
 
   module.exports = factorial;
