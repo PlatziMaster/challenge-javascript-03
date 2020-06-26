d1a180168dafb58e8513527d7863ac2889bc0a23
Moilu <moisesluna0390@gmail.com>
2020-06-25 21:03:30 -0700

    Factorial completado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const factorial = (number) => {
-    
+
+    if (number == 0)
+    {
+      return 1;
+    }
+    else
+    {
+      factorial = number * factorial(number -1);
+      return factorial;
+    }
   }
 
   module.exports = factorial;
