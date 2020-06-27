c9c412b0f04484e8921cc29296001fce7bbdd311
Angel Castiblanco <angelcastiblanco96@gmail.com>
2020-06-27 00:00:42 -0500

    Completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
-    
+  let result = number
+  if(number === 1 || number === 0){
+    return 1
+  } else {
+    for (let i = number; i > 1; i--) {
+      result *= (i - 1)
+    }
   }
+  return result
+}
 
   module.exports = factorial;
