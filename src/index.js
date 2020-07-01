44265cde42fd6151ad976cf9615621eaa4410f63
Daniel Valderrama <dny9919@gmail.com>
2020-07-01 13:57:24 -0500

    challenge #03 ready

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-const factorial = (number) => {
-    
-  }
-
-  module.exports = factorial;
+const factorial = (n) => {
+  var resultadoFactorial = 1
+  for (let i = 1; i <= n; i++) {
+    resultadoFactorial = resultadoFactorial * i;
+  };
+  console.log(resultadoFactorial);
+  
+  return resultadoFactorial 
+}
+factorial();
+module.exports = factorial;
