9d64269b56d4827a391d8ad2e45e305f13f2cb31
Japh <japhethcalzada@gmail.com>
2020-07-06 13:55:45 -0500

    factorial exercise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const factorial = (number) => {
+    const factorialnNumber = number ; 
+    var returnFactorial = 1; 
+    for (let x =1; x <= factorialnNumber; x ++){
+      returnFactorial *=x;
+    }
+    return returnFactorial; 
     
   }
+
 
   module.exports = factorial;
