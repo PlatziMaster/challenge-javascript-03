7e9d75dfe5ef5bc9994f20db196adfd13f29372d
Ulzahk <ulzahk@gmail.com>
2020-07-08 23:34:28 -0500

    Challenge's Answer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const factorial = (number) => {
-    
-  }
+    var accumulator = 1;
+    for(var i = number; i > 0; i--){
+      accumulator *= i;
+    }
+    return accumulator;
+}
 
   module.exports = factorial;
