4635f2ce549423ee8b17cb59d43b980406a09345
chato1337 <chato1337@gmail.com>
2020-07-09 21:10:06 -0500

    solucion al reto por didier cuetia

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const factorial = (number) => {
+    console.log(number);
+    var factorial = 1;
     
+    for (let i = 1; i <= number; i++) {
+      factorial = factorial * i;  
+    }
+
+    return factorial;
   }
 
   module.exports = factorial;
