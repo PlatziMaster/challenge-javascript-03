70ab96ea3ffa5614f683c3ddf7edd01f64216e5c
Alexander Herrera <chander.zap@gmail.com>
2020-07-18 00:31:34 -0500

    Reto terminado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
-    
+  return fact(number)
+}
+
+function fact(number){
+  if (number > 0){
+    return number * fact(number - 1);
+  }else{
+    return 1;
   }
+}
 
   module.exports = factorial;
