9d47f226579801d2233ea6f8e9fb550520083074
Paul Montoya <52388061+PaulMoro@users.noreply.github.com>
2020-07-18 23:29:15 -0500

    finish challenge 03 Javascript

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
-    
+  // number = 5
+  // 5 = 5 * 4 * 3 * 2 = 120
+  // n = (n-i) * (n-i) * (n-i) * (n-i)
+  // n = (n-(n)) * (n-(n-1)) * (n-(n-2)) * (n-i)
+  if(number <= 1) {
+    return 1
   }
-
+  else {
+    return number * factorial(number - 1)
+  }
+}
   module.exports = factorial;
