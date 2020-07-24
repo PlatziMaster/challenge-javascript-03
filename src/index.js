4ce51f4bc2aaf5faf0f77479ee5a52f7b2516136
Pitupan <rojas.fredic6@gmail.com>
2020-07-24 09:15:39 -0500

    Realizacion de ejercicio de factorial para el challenge-3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const factorial = (number) => {
-    
+	let factorialArray=[];
+	var i = number;
+	var factorialValue;
+    if(i<0){
+		console.log(`${number} is not a factorial number`);
+	}else if(i===0){
+			var factorialValue = 1;
+		}else{
+				while(i>0){
+						factorialArray.push(i);
+						i--;
+						var factorialValue = factorialArray.reduce(function(acumulador, init){
+			acumulador = acumulador * init;
+			return acumulador;
+			return factorialValue;
+					}
+				);
+			};
+	}
+	return factorialValue;
   }
 
+console.log(factorial(2));
   module.exports = factorial;
