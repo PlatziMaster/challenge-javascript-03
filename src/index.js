c712b3bc63467ff983a08d430ba3985478346458
Jobana Garavito Q <nellyjobana@gmail.com>
2020-07-26 18:20:36 -0500

    Factorial excercise by Jobana Garavito

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const factorial = (number) => {
-    
+    let numbers = []
+    for (i=1; i<=number; i++)
+    numbers.push(i)
+    const reducer = (acum, numbers) => acum * numbers
+    factor = numbers.reduce(reducer,1)
+    return factor
   }
 
   module.exports = factorial;
+
