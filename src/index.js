27444ce2652648817fa5f6ccadbb4bf0a0a83e43
JoelNieto <joenieto90@hotmail.com>
2020-07-29 01:31:08 -0500

    Challenge Javascript 03 Solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const factorial = (number) => {
-    
+    var fact = number;
+    if(fact === 0 || fact === 1)
+    {
+      return 1;
+    }
+    return fact * factorial(fact-1);
   }
 
   module.exports = factorial;
