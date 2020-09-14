fa7985053326016a48179c08f67b93c9e0c3e015
Octavio Cuellar Vargas <octaviocvargas@gmail.com>
2020-09-13 20:18:20 -0500

    Mi primer reto javaScript

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const factorial = (number) => {
-    
+  var numero = number
+  var resultado = 1
+  for(var i=1; i<=numero; i++){
+      resultado = resultado * i
+  }
+  return resultado;
   }
 
   module.exports = factorial;
