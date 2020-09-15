464a05b591a99035813e9805194102b6ac175df6
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-09-14 23:55:50 -0500

    Challenge JavaScript 03 resolved

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const factorial = (number) => {
   if (number <= 1){
     return 1; 
   } else {
-  let answer = 1
+  let answer = 1;
   for(let i=2; i<=number; i++){
-      answer = answer * i;
+    answer = answer * i;
   }
   return answer;
   }
