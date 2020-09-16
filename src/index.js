b09473736802d1eaaa0f781ab29494d357b7c49e
Efrain Hernandez <efrainhg2050@gmail.com>
2020-09-15 22:27:45 -0500

    Solución al reto Factorial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const factorial = (number) => {
-
-    for(let i = number-1; i > 0; i--) {
-      number *= i;
+  
+    if(number === 0) {
+      return 1;
+    } else {
+      for(let i = number-1; i > 0; i--) {
+        number *= i;
+      }
+      return number;
     }
-    return number;
+
   }
 
   module.exports = factorial;
