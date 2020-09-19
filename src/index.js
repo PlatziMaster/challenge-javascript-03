bcdda2ef14267de51ce2ba8b8dd93348861895c4
Israel Castro <israel.castro@gmail.com>
2020-09-19 03:06:57 -0500

    [Donde] Challenge JS 03

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-const factorial = (number) => {
-    
-  }
+const factorial = (number) => (number < 2 ? 1 : factorial(number - 1) * number);
 
-  module.exports = factorial;
+module.exports = factorial;
