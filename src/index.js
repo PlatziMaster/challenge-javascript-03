93d128ded8ed792a6def910d02db6a5ad0b32619
williboss <santiago_alza@hotmail.com>
2021-04-27 20:41:05 -0500

    Challenge javascript 03 - Platzi Master, done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const factorial = (number) => {
-    
-  }
+  if (number >= 0) {
+    let factorial = 1;
+    for (let index = 2; index <= number; index++) {
+      factorial = factorial * index;
+    }
+    return factorial;
+  } else {
+    return 0;
+  }    
+}
 
-  module.exports = factorial;
+module.exports = factorial;
